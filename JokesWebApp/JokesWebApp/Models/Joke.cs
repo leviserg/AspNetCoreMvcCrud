e4d214fa128ba5a1994b9d05b208128d8094332a
AUX502655 <AUX502655@prrft.refinitiv.com>
2021-10-24 22:25:11 +0300

    github upload

---
+++ JokesWebApp/JokesWebApp/Models/Joke.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public Microsoft.AspNetCore.Identity.IdentityUser AplicationUser { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+
+    public class ModelExt
+    {
+        public int JokeId { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public string UserName { get; set; }
+
+        public ModelExt()
+        {
+
+        }
+    }
+}
